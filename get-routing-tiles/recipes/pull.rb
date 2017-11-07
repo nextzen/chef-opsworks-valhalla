7746b59046aa725c13e27cbcd5b2691dcabb79f1
Grant Heffernan <heffergm@gmail.com>
2017-11-07 08:02:23 -0500

    pull

--- get-routing-tiles/recipes/pull.rb
+++ get-routing-tiles/recipes/pull.rb
@@ -17,8 +17,8 @@ execute 'pull tiles' do
   user  node['get-routing-tiles']['user']['name']
   cwd   node['get-routing-tiles']['base_dir']
   command <<-EOH
-    aws --region #{stack['region']} s3 ls s3://#{node['get-routing-tiles']['s3bucket']}/#{node['get-routing-tiles']['s3bucket_dir']}/ | grep -F planet_ | awk '{print $4}' | sort | tail -n 1 > latest_tiles.txt &&
-    aws --region #{stack['region']} s3 cp s3://#{node['get-routing-tiles']['s3bucket']}/#{node['get-routing-tiles']['s3bucket_dir']}/$(cat latest_tiles.txt) #{node['get-routing-tiles']['tile_dir']}/tiles.tar
+    aws --region #{stack['region']} s3 ls s3://#{node['get-routing-tiles']['s3bucket']}/#{node['get-routing-tiles']['s3bucket_dir']}/ | grep -F planet_ | awk '{print $2}' | sort | tail -n 1 > latest_tiles.txt &&
+    aws --region #{stack['region']} s3 cp s3://#{node['get-routing-tiles']['s3bucket']}/#{node['get-routing-tiles']['s3bucket_dir']}/$(cat latest_tiles.txt)$(cat latest_tiles.txt | cut -d'/' -f 1).tar #{node['get-routing-tiles']['tile_dir']}/tiles.tar
   EOH
   timeout 3600
 end
